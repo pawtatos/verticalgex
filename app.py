04f9171b733783ca285a1f91057b9153f5e46e97
pawtatos <xjackliu28@gmail.com>
2026-02-01 23:43:22 -0800

    Update app.py

--- app.py
+++ app.py
@@ -331,7 +331,7 @@ def render_mini_cards(title: str, df: pd.DataFrame, cols_desktop: int = 3):
         ">
           <div style="min-width:0;">
             <div style="
-              font-size:16px;
+              font-size:14px;
               font-weight:800;
               line-height:1.10;
               color:#e6e6e6;
@@ -362,7 +362,7 @@ def render_mini_cards(title: str, df: pd.DataFrame, cols_desktop: int = 3):
             min-width:80px;
           ">
             <div style="
-              font-size:14px;
+              font-size:12px;
               font-weight:900;
               color:#e6e6e6;
               line-height:1.0;
@@ -968,6 +968,7 @@ with right:
         chart_title = f"{ticker} - All expiries"
 
     render_chart(gex_all=gex_all, spot=spot, chart_title=chart_title)
+
 
 
 
